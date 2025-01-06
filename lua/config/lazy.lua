7e61ebb8b543cc04e2432c263b4701671a84e699
JS Lim <jslim89@gmail.com>
2025-01-06 14:06:42 +0800

    Suppress the plugins update message

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "seoul256" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
