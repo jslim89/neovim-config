5ad8a49317049bb8e2b397bf98f8a6d790e8c0b3
Js Lim <jslim89@gmail.com>
2025-08-03 23:42:41 +0800

    add keymap

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -11,6 +11,13 @@ local opts = { noremap = true, silent = true }
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
+-- remove trailing spaces
+keymap("n", "<leader>rs", function()
+  local saved_search = vim.fn.getreg("/")
+  vim.cmd("%s/\\s\\+$//e")
+  vim.fn.setreg("/", saved_search)
+end, opts)
+
 -- VS Code specific actions --
 keymap("n", "<leader>n", "<cmd>lua require('vscode').action('workbench.action.toggleSidebarVisibility')<CR>", opts)
 keymap("n", "<leader>r", "<cmd>lua require('vscode').action('editor.action.rename')<CR>", opts)
